0a0fefd050039da0307ece24c5992dbaa8d73a47
SaumoB <saumobanik@gmail.com>
2025-08-27 13:20:14 +0530

    add static site

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,11 @@
 // export default defineConfig({
 //   plugins: [react()],
 // })
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-})
-
+  plugins: [react()],
+  base: "/menu-creater/", // 👈 repo name
+});
 
